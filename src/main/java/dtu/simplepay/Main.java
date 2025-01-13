8c8493ad093078095f463b8cf966e43d55336991
asher <asher.sharif@xintsolutions.com>
2025-01-13 14:46:49 +0100

    reverting back

--- src/main/java/dtu/simplepay/Main.java
+++ src/main/java/dtu/simplepay/Main.java
@@ -8,6 +8,5 @@ public class Main {
     public static void main(String[] args) {
         // SpringApplication.run(Main.class, args);
         // System.out.println("SimpleDTUPay Application is running...");
-        // commneted to check 
     }
 }
